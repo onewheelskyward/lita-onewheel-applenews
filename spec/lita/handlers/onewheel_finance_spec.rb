10f29eaecdba4bdd615ae867bb082d6ce20b658d
Andrew Kreps <andrew.kreps@gmail.com>
2020-01-02 01:22:01 -0800

    Added a test for down.

--- spec/lita/handlers/onewheel_finance_spec.rb
+++ spec/lita/handlers/onewheel_finance_spec.rb
@@ -10,10 +10,18 @@ describe Lita::Handlers::OnewheelFinance, lita_handler: true do
   #
   #end
 
-  it 'quotes' do
+  it 'quotes up' do
     mock_up 'alphavantage-global-quote'
     send_command 'quote lulu'
     puts replies.last
     expect(replies.last).to include('LULU: $230.83')
+  end
+
+  it 'quotes down' do
+    mock_up 'alphavantage-quote-down'
+    send_command 'quote clb'
+    puts replies.last
+    expect(replies.last).to include('CLB: $37.67')
+    expect(replies.last).to include('$-9.79')
   end
 end
