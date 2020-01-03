c3f895a6528878b1a32157624940a1161b613c7e
Andrew Kreps <andrew.kreps@gmail.com>
2020-01-02 16:25:11 -0800

    Added did you mean v1

--- lib/lita/handlers/onewheel_finance.rb
+++ lib/lita/handlers/onewheel_finance.rb
@@ -68,7 +68,7 @@ class AlphaVantageQuote
 end
 
 class WorldTradeDataQuote
-  attr_reader :open, :high, :low, :price, :volume, :trading_day, :prev_close, :change, :change_percent, :exchange, :error, :name
+  attr_reader :open, :high, :low, :price, :volume, :trading_day, :prev_close, :change, :change_percent, :exchange, :error, :name, :message
   attr_accessor :symbol
 
   def initialize(symbol, api_key)
@@ -84,9 +84,15 @@ class WorldTradeDataQuote
     if hash['Message'].to_s.include? 'Error'
       @error = true
       self.run_search
-      self.call_api
-      hash = JSON.parse(@response)
-      @error = false
+
+      if @message
+        @error = true
+        return
+      else
+        self.call_api
+        hash = JSON.parse(@response)
+        #@error = false
+      end
     else
       @error = false
     end
@@ -149,62 +155,15 @@ class WorldTradeDataQuote
 
     response = RestClient.get url, {params: params}
 
-    Lita.logger.debug "response: #{@response}"
+    Lita.logger.debug "response: #{response}"
     result = JSON.parse(response)
 
     if result['total_returned'] == 1
       @symbol = result['data'][0]['symbol']
-    end
-  end
-
-  def fix_number(price_str)
-    price_str.to_f.round(2)
-  end
-end
-
-class WorldTradeDataSearch
-  attr_reader :open, :high, :low, :price, :volume, :trading_day, :prev_close, :change, :change_percent, :exchange, :error, :name
-
-  def initialize(json_blob)
-    Lita.logger.debug "parsing: #{json_blob}"
-    hash = JSON.parse(json_blob)
-
-    if hash['Message'].to_s.include? 'Error'
-      @error = true
-      return
-    else
-      @error = false
-    end
-
-    quote = hash['data'][0]
-
-    quote.keys.each do |key|
-      case key
-      when "symbol"
-        @symbol = quote[key]
-      when "price_open"
-        @open = self.fix_number quote[key]
-      when "day_high"
-        @high = self.fix_number quote[key]
-      when "day_low"
-        @low = self.fix_number quote[key]
-      when "price"
-        @price = self.fix_number quote[key]
-      when "volume"
-        @volume = quote[key].to_i
-      when "last_trade_time"
-        @trading_day = quote[key]
-      when "08. previous close"
-        @prev_close = self.fix_number quote[key]
-      when "day_change"
-        @change = self.fix_number quote[key]
-      when "change_pct"
-        @change_percent = self.fix_number quote[key]
-      when 'stock_exchange_short'
-        @exchange = quote[key].sub /NYSEARCA/, 'NYSE'
-      when 'name'
-        @name = quote[key]
-      end
+    elsif result['total_returned'] > 1
+      Lita.logger.debug "many search results: #{result.inspect}"
+      x = result['data'].map { |k| k.values[0] }
+      @message = "`#{symbol}` not found, did you mean one of #{x.join(', ')}?"
     end
   end
 
@@ -217,13 +176,17 @@ module Lita
   module Handlers
     class OnewheelFinance < Handler
       config :apikey, required: true
-      route /quote\s+(.+)/i, :handle_quote, command: true
+      route /qu*o*t*e*\s+(.+)/i, :handle_quote, command: true
 
       def handle_quote(response)
         stock = handle_world_trade_data response.matches[0][0]
 
         if stock.error
-          str = "`#{stock.symbol}` not found on any stock exchange."
+          if stock.message
+            str = stock.message
+          else
+            str = "`#{stock.symbol}` not found on any stock exchange."
+          end
         else
           str = "#{IrcColors::grey}#{stock.exchange} - #{IrcColors::reset}#{stock.symbol}: #{IrcColors::blue}$#{stock.price}#{IrcColors::reset} "
           if stock.change >= 0
@@ -248,13 +211,13 @@ module Lita
       end
 
       # deprecated for now
-      def handle_alphavantage
-        url = "https://www.alphavantage.co/query"
-        params = {function: 'GLOBAL_QUOTE', symbol: response.matches[0][0], apikey: config.apikey}
-        Lita.logger.debug "#{url} #{params.inspect}"
-        resp = RestClient.get url, {params: params}
-        stock = GlobalQuote.new resp
-      end
+      #def handle_alphavantage
+      #  url = "https://www.alphavantage.co/query"
+      #  params = {function: 'GLOBAL_QUOTE', symbol: response.matches[0][0], apikey: config.apikey}
+      #  Lita.logger.debug "#{url} #{params.inspect}"
+      #  resp = RestClient.get url, {params: params}
+      #  stock = GlobalQuote.new resp
+      #end
 
       Lita.register_handler(self)
     end
