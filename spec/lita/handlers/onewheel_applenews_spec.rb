486c5c2b2ebd03be66a47e322b783ea1f48e3ee6
Andrew Kreps <andrew.kreps@gmail.com>
2021-05-15 09:38:11 -0700

    Tests!

--- spec/lita/handlers/onewheel_applenews_spec.rb
+++ spec/lita/handlers/onewheel_applenews_spec.rb
@@ -1,15 +1,14 @@
 require 'spec_helper'
 
 def mock_up(filename)
-  mock = File.open("spec/fixtures/#{filename}.json").read
+  mock = File.open("spec/fixtures/#{filename}.html").read
   allow(RestClient).to receive(:get) { mock }
 end
 
 describe Lita::Handlers::OnewheelApplenews, lita_handler: true do
   it 'quotes up' do
-    mock_up 'worldtradedata-quote-up'
-    send_command 'quote lulu'
-    expect(replies.last).to include("\u000314NASDAQ - \u0003LULU: \u000302$233.01\u0003")
-    expect(replies.last).to include('(Lululemon Athletica Inc.)')
+    mock_up 'fil'
+    send_message 'https://apple.news/ALjDjYg-3QKuFStFxUMRpZA'
+    expect(replies.last).to include('https://www.cnn.com/2021/05/03/tech/bill-and-melinda-gates-divorce/index.html')
   end
 end
