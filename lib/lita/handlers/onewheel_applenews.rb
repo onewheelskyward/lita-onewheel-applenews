480786a89000f78510c8ae6da3def8ade1852b78
Andrew Kreps <andrew.kreps@gmail.com>
2022-07-20 11:15:36 -0700

    fixing an issue with pre-pending text.

--- lib/lita/handlers/onewheel_applenews.rb
+++ lib/lita/handlers/onewheel_applenews.rb
@@ -3,8 +3,8 @@ require 'rest-client'
 module Lita
   module Handlers
     class OnewheelApplenews < Handler
-      route /(.*apple.news\/[^\s]+)/i, :handle_applenews
-      route /(.*stocks.apple.com\/[^\s]+)/i, :handle_applenews
+      route /(http.*apple.news\/[^\s]+)/i, :handle_applenews
+      route /(http.*stocks.apple.com\/[^\s]+)/i, :handle_applenews
 
       def handle_applenews(response)
         Lita.logger.info("Fetching #{response.matches[0][0]}")
